423afea2ca8cdaf8b498ca437fe5a5dfc0c90da1
Dhaval Purohit <dhaval.purohit08@gmail.com>
2014-11-16 17:07:29 +0530

    Created project app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AscDavv::Application.config.secret_token = '8ab9031e6b03465cc6f147df426493abd1875a652a17a840baa8711a8960b6a6d1485397b21a036b118f0b09f77144dd2b5de8169ea4eba04b4295e6d59fe977'
