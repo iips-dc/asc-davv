0acb61eb8e657931ca74e10fdc3d0560ad1edb7b
Dhaval Purohit <dhaval.purohit08@gmail.com>
2015-01-12 17:22:16 +0530

    Updated course model and Created course controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150107101250) do
+ActiveRecord::Schema.define(:version => 20150112111810) do
 
   create_table "courses", :force => true do |t|
     t.string   "course_name"
