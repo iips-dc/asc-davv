c35085d1516055d08373b4354d5a25150a7a38ca
Dhaval Purohit <dhaval.purohit08@gmail.com>
2015-04-16 08:29:44 +0530

    Adds searching, sorting and filter for modules

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -3,22 +3,36 @@ class FeedbacksController < ApplicationController
   before_filter :authenticate_admin!
   load_and_authorize_resource
   layout "adminDashboard"
-  respond_to :html, :json
 
   def index
-    @feedbacks = Kaminari.paginate_array(Feedback.all).page(params[:page]).per(25)
+    @filterrific = initialize_filterrific(
+    Feedback,
+    params[:filterrific],
+    select_options: {
+        sorted_by: Feedback.options_for_sorted_by
+    },
+    persistence_id: 'shared_key',
+    default_filter_params: { sorted_by: 'created_at_desc' },
+    available_filters: [ 
+        :sorted_by ],
+    ) or return
+
+    @feedbacks = Kaminari.paginate_array(@filterrific.find).page(params[:page]).per(25)
   end
 
   def show
+    respond_to :html, :json
     respond_with(@feedback)
   end
 
   def new
     @feedback = Feedback.new
+    respond_to :html, :json
     respond_with(@feedback)
   end
 
   def edit
+    respond_to :html, :json
   end
 
   def create
@@ -37,20 +51,24 @@ class FeedbacksController < ApplicationController
 
   def update
     @feedback.update_attributes(params[:feedback])
+    respond_to :html, :json
     respond_with(@feedback)
   end
 
   def destroy
     @feedback.destroy
+    respond_to :html, :json
     respond_with(@feedback)
   end
 
   def report_iframe
+    respond_to :html, :json
     render :layout => false
   end 
 
   private
     def set_feedback
+      respond_to :html, :json
       @feedback = Feedback.find(params[:id])
     end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -27,3 +27,71 @@ class Course < ActiveRecord::Base
     end
   end
 end  
+
+# searching and sorting
+class Course < ActiveRecord::Base
+    filterrific(
+      default_filter_params: { sorted_by: 'created_at_desc' },
+      available_filters: [
+        :sorted_by, 
+        :search_query,
+        :with_session
+        ]
+    )
+
+    scope :search_query, lambda { |query|
+      return nil if query.blank?
+
+      # condition query, parse into individual keywords
+      terms = query.to_s.downcase.split(/\s+/)
+
+      # replace "*" with "%" for wildcard searches,
+      # append '%', remove duplicate '%'s
+      terms = terms.map { |e|
+        ('%' + e.gsub('*', '%') + '%').gsub(/%+/, '%')
+      }
+
+      # configure number of OR conditions for provision
+      # of interpolation arguments. Adjust this if you
+      # change the number of OR conditions.
+      num_or_conditions = 2
+      where(
+        terms.map {
+          or_clauses = [
+            "LOWER(courses.course_name) LIKE ?",
+            "LOWER(courses.course_type) LIKE ?"
+          ].join(' OR ')
+          "(#{ or_clauses })"
+        }.join(' AND '),
+        *terms.map { |e| [e] * num_or_conditions }.flatten
+      )
+    }
+
+    scope :sorted_by, lambda { |sort_option|
+      # extract the sort direction from the param value.
+      direction = (sort_option =~ /desc$/) ? 'desc' : 'asc'
+      case sort_option.to_s
+      when /^created_at_/
+        # Simple sort on the created_at column.
+        # Make sure to include the table name to avoid ambiguous column names.
+        # Joining on other tables is quite common in Filterrific, and almost
+        # every ActiveRecord table has a 'created_at' column.
+        order("courses.created_at #{ direction }")
+      else
+        raise(ArgumentError, "Invalid sort option: #{ sort_option.inspect }")
+      end
+    }
+
+    def self.options_for_sorted_by
+      [
+        ['Newest first', 'created_at_desc'],
+        ['Oldest first', 'created_at_asc']
+      ]
+    end
+
+
+    scope :with_session, lambda { |session|
+      where(session: [*session])
+    }
+
+end 
