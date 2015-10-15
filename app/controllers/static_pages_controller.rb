e79a9e15e9eaaeb6d427e5e26d5430dc7e0eb787
Dhaval Purohit <dhaval.purohit08@gmail.com>
2015-10-15 17:29:35 +0530

    moves dd receipt generation in backend

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+	before_filter :authenticate_admin!, :only => [:dd_result,:dd_search]
+	load_and_authorize_resource :class => false, :only => [:dd_result,:dd_search]
 	respond_to :html
 
 	def index
@@ -14,22 +16,28 @@ class StaticPagesController < ApplicationController
 		respond_with(@courses)
 	end	
 
+	def dd_search
+		respond_to do |format|
+			format.html { render "/static_pages/dd_search", :layout => "adminDashboard"}
+		end	
+	end	
+
 	def dd_result
-		date=Date.parse(params[:dob].to_s).strftime('%Y-%m-%d')
 		@receipt_date=Date.parse(params[:receipt_date].to_s).strftime('%d-%b-%Y')
 		@course_type=params[:course_type]
 		@certificate_amt=params[:certificate][0].to_i
 		@others=params[:others][0].to_i
+		
 		if @course_type == 'Short-term Course'
-			@record=ShorttermCourse.find_by_dob_and_id(date, params[:registration_id])
+			@record=ShorttermCourse.find(params[:registration_id])
 		elsif @course_type == 'Refresher Course'
-			@record=RefresherCourse.find_by_dob_and_id(date, params[:registration_id])
+			@record=RefresherCourse.find(params[:registration_id])
 		elsif @course_type == 'Orientation Course'
-			@record=OrientationCourse.find_by_dob_and_id(date, params[:registration_id])
+			@record=OrientationCourse.find(params[:registration_id])
 		elsif @course_type == 'Interaction Program'
-			@record=InteractionProgram.find_by_dob_and_id(date, params[:registration_id])
+			@record=InteractionProgram.find(params[:registration_id])
 		elsif @course_type == 'Principal Workshop'
-			@record=PrincipalWorkshop.find_by_dob_and_id(date, params[:registration_id])	
+			@record=PrincipalWorkshop.find(params[:registration_id])
 		end
 
 		if @record.nil?
@@ -38,7 +46,7 @@ class StaticPagesController < ApplicationController
 			end	
 		else	
 			respond_to do |format|
-				format.html { render "/static_pages/dd_result", :layout => false}
+				format.html { render "/static_pages/dd_result", :layout => "receipt"}
 			end		
 		end	
 	end
@@ -64,7 +72,7 @@ class StaticPagesController < ApplicationController
 			end	
 		else	
 			respond_to do |format|
-				format.html { render "/static_pages/result", :layout => false}
+				format.html { render "/static_pages/result", :layout => "receipt"}
 			end		
 		end	
 	end
